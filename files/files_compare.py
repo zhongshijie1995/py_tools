e6c4c8d03a583f6c573de2abd526894a845da439
zhongshijie <zhongshijie1995@outlook.com>
2022-04-23 21:19:33 +0800

    CodeReview

--- files/files_compare.py
+++ files/files_compare.py
@@ -16,9 +16,10 @@ def get_file_md5(file_path: str) -> str:
     return md5
 
 
-def paths_compare(paths_num: int = 2):
+def paths_compare(paths_num: int = 2) -> dict:
     paths = []
     dirs_dict = dict({})
+    compare_dict = dict({})
     for i in range(paths_num):
         target_dir = input('请输入第%d组目录：' % i)
         paths.append(target_dir)
@@ -29,23 +30,28 @@ def paths_compare(paths_num: int = 2):
                 sub_file_path = '.' + file_path.strip(target_dir)
                 dirs_dict[target_dir][sub_file_path] = get_file_md5(file_path)
     for (i, j) in list(itertools.permutations(range(paths_num), 2)):
-        print('第%d组目录比第%d组目录多的文件：' % (i, j))
         result = {x: x not in dirs_dict[paths[j]] for x in dirs_dict[paths[i]]}
+        tmp = []
         for k, v in result.items():
             if v:
-                print(k)
-        print('--------------')
+                tmp.append(k)
+        compare_dict['第%d组目录 - 第%d组目录：' % (i, j)] = tmp
     for (i, j) in list(itertools.combinations(range(paths_num), 2)):
-        print('第%d组目录与第%d组目录中内容不同的文件：' % (i, j))
         result = {x: dirs_dict[paths[i]] != dirs_dict[paths[j]] for x in dirs_dict[paths[i]] if x in dirs_dict[paths[j]]}
+        tmp = []
         for k, v in result.items():
             if v:
-                print(k)
-        print('--------------')
+                tmp.append(k)
+        compare_dict['第%d组文件 != 第%d组文件：' % (i, j)] = tmp
+    return compare_dict
 
 
 def main():
-    paths_compare()
+    for k, v in paths_compare().items():
+        print(k)
+        for i in v:
+            print(v)
+        print('-------------------------------')
 
 
 if __name__ == '__main__':
